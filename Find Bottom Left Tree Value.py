036a58be116db8bf6f591bfce8b416405ef97bed
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-02-28 10:31:07 +0530

    Create Find Bottom Left Tree Value.py

---
+++ Find Bottom Left Tree Value.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return None
+        
+        queue = deque([root])
+        leftmost_value = None
+        
+        while queue:
+            level_size = len(queue)
+            leftmost_value = queue[0].val  # Store the leftmost value of the current level
+            
+            for _ in range(level_size):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return leftmost_value
