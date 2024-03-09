c2ed2c3cb7ee2a49607d0fc97983f34e52b1796e
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-03-09 23:36:26 +0530

    Create Minimum Common Value.py

---
+++ Minimum Common Value.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getCommon(self, nums1, nums2):
+        ptr1 = ptr2 = 0
+        while ptr1 < len(nums1) and ptr2 < len(nums2):
+            if nums1[ptr1] == nums2[ptr2]:
+                return nums1[ptr1]
+            elif nums1[ptr1] < nums2[ptr2]:
+                ptr1 += 1
+            else:
+                ptr2 += 1
+        return -1
